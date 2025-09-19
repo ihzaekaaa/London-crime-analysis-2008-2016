5b6a84f46e9b20e88e96b69a6b6b0aee5065841e
Ihza Eka <167768495+ihzaekaaa@users.noreply.github.com>
2025-09-19 15:53:18 +0700

    Delete major_category.sql

--- major_category.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  major_category,
-  month,
-  year,
-  SUM(value) AS total_cases
-FROM `bigquery-public-data.london_crime.crime_by_lsoa`
-WHERE year >= 2012
-GROUP BY major_category, month, year
-ORDER BY year, month, major_category;
